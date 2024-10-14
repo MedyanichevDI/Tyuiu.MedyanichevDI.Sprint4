2cfb3bcd97cc98c6a581ca8cda2435217d628955
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-10-14 12:46:51 +0500

    Task0

---
+++ Tyuiu.MedyanichevDI.Sprint4.Task0.V23.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.MedyanichevDI.Sprint4.Task0.V23.Lib
+{
+    public class DataService : ISprint4Task0V23
+    {
+        public int GetMultOddArrEl(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++) 
+            { 
+                if (array[i] %2!= 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.MedyanichevDI.Sprint4.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MedyanichevDI.Sprint4.Task0.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
