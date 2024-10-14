875f31167a0ed3256edf6ab6cae69ec152e71c59
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-10-14 12:50:37 +0500

    Task0

--- Tyuiu.MedyanichevDI.Sprint4.Task0.V23.Lib/DataService.cs
+++ Tyuiu.MedyanichevDI.Sprint4.Task0.V23.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.MedyanichevDI.Sprint4.Task0.V23.Lib
             int sum = 0;
             for (int i = 0; i < array.Length; i++) 
             { 
-                if (array[i] %2!= 0)
+                if (array[i] %2> 0)
                 {
                     sum += array[i];
                 }
