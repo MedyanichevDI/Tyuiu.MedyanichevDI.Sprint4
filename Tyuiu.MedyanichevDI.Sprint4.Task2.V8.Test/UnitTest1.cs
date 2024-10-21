bb844e2ad9cf3d1170fbf7c90ed59b46974df700
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-10-21 20:42:35 +0500

    Task2

---
+++ Tyuiu.MedyanichevDI.Sprint4.Task2.V8.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MedyanichevDI.Sprint4.Task2.V8.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MedyanichevDI.Sprint4.Task2.V8/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.MedyanichevDI.Sprint4.Task2.V8.Lib;
+
+namespace Tyuiu.MedyanichevDI.Sprint4.Task2.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Медяничев Д.И. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы                                                *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Медяничев Денис Игоревич | АСОиУб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("Дан одномерный целочисленный массив на 10 элементов заполненный           *");
+
+            Console.WriteLine("*статическими значениями в диапазоне от 0 до 9                            *");
+            Console.WriteLine("*подсчитать произведение нечетных элементов массива.                      *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("                                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Random rnd = new Random();
+            int len = 11;
+            int[] arr = new int[len];
+            for (int i=0; i <= len; i++)
+            {
+                arr[i] = rnd.Next(2, 7);
+            }
+            for (int i = 0; i < len-1; i++)
+            {
+                Console.WriteLine(arr[i] + "/t");    
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(arr));
+        }
+    }
+}
