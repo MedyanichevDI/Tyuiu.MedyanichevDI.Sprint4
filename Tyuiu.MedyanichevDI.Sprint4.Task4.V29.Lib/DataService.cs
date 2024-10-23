b93c87f4a3d06dae80161823c1823893eee29b12
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-10-23 20:09:22 +0500

    Task4

---
+++ Tyuiu.MedyanichevDI.Sprint4.Task4.V29.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MedyanichevDI.Sprint4.Task4.V29.Lib
+{
+    public class DataService : ISprint4Task4V29
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int sum = 0;
+            int row = matrix.GetUpperBound(0) + 1;
+            int colum = matrix.Length / row;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < colum; j++)
+                {
+                    if (matrix[i,j]%2== 0)
+                    {
+                        sum += matrix[i, j];
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.MedyanichevDI.Sprint4.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MedyanichevDI.Sprint4.Task4.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
