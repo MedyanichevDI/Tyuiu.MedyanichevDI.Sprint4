2ac4891ee575177cddc245287a41f655ed972598
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-10-23 18:31:46 +0500

    Task3

---
+++ Tyuiu.MedyanichevDI.Sprint4.Task3.V17.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MedyanichevDI.Sprint4.Task3.V17.Lib
+{
+    public class DataService : ISprint4Task3V17
+    {
+        public int Calculate(int[,] array)
+        {
+            int sum = 0;
+            int row = array.GetUpperBound(0) + 1;
+            int colum = array.Length / row;
+            for (int i=0; i < row; i++)
+            {
+                for (int j=0; j<colum; j++)
+                {
+                    if (j == 4)
+                    {
+                        sum += array[i, j];
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.MedyanichevDI.Sprint4.Task3.V17/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.MedyanichevDI.Sprint4.Task3.V17.Lib;
+
+namespace Tyuiu.MedyanichevDI.Sprint4.Task3.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Медяничев Д.И. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы                                                *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #17                                                              *");
+            Console.WriteLine("* Выполнил: Медяничев Денис Игоревич | АСОиУб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("Дан двумерный целочисленный массив 5 на 5 элементов, заполненный          *");
+
+            Console.WriteLine("*в четвертом столбце массива.   *");
+            //Console.WriteLine("*подсчитать произведение нечетных элементов массива.                      *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("                                                                          *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int[,] arr =  { { 6, 4, 2, 2, 1 },
+                            { 3, 6, 5, 4, 1 },
+                            { 5, 2, 3, 1, 6 },
+                            { 8, 8, 4, 5, 3 },
+                            { 7, 4, 5, 1, 6 } };
+
+            for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < arr.GetUpperBound(1) + 1; i++)
+                {
+                    Console.WriteLine(arr[i, j] + "/t");
+                }
+
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(arr));
+        }
+    }
+}
